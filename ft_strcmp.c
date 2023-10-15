9a4c41fa6360e8b7b65051c6250ec122bc17e515
Wbelyne <joaquim1810@icloud.com>
2023-10-15 10:11:08 -0400

    strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -2,10 +2,14 @@
 
 int ft_strcmp(char *s1, char *s2)
 {
-    // int i = 0;
-    // while (str[i])
-    // {
-    //     /* code */
-    // }
-    
+    int i = 0;
+   if (!s1 || !s2)
+        return 0;
+    while (s1[i], s2[i])
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (0);
 }
