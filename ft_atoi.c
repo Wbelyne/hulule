2ff42d69723d3214297ea38b6160271823c60a81
Wbelyne <joaquim1810@icloud.com>
2023-10-21 12:40:44 -0400

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,4 +3,20 @@
 int ft_atoi(char *str)
 {
     
+    int i = 0;
+    int sign = 1;
+    if (*str == 0)
+        return (0);
+    while (*str)
+    {
+        if (*str == '-')
+            sign = - 1;
+        if (*str == '+')
+            sign = 1;
+        if (*str >= '0' && *str <= '9')
+            i = i * 10 + (*str - '0');
+        str++;
+    }
+     ft_putnbr(i * sign);
+    return (i * sign);
 }
