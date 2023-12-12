9268efd0ac4c3749b2576fe261084dcb4bc0b0e2
crystal <crystal@crystal.crystal>
2023-12-12 14:43:23 +0100

    putnbrbase

--- ft_iterative_power.c
+++ ft_iterative_power.c
@@ -16,7 +16,7 @@ int ft_iterative_power(int nb, int power)
 
 int main(int argc, char const *argv[])
 {
-   printf("0:%d\n", ft_iterative_power(1, -10));
+    printf("0:%d\n", ft_iterative_power(1, -10));
 	printf("0:%d\n", ft_iterative_power(1, -1));
 	printf("1:%d\n", ft_iterative_power(10, 0));
 	printf("10:%d\n", ft_iterative_power(10, 1));
