c8107d1bfb08c0ac761a5f35dffc29dc109643d1
crystal <crystal@crystal.crystal>
2023-12-12 14:42:04 +0100

    vm

--- ft_iterative_power.c
+++ ft_iterative_power.c
@@ -2,11 +2,24 @@
 
 int ft_iterative_power(int nb, int power)
 {
-
+    int baisse = 1;
+    if (nb && power == 0)
+        return 1;
+    
+    while(baisse != power)
+    {
+        nb *= nb;
+        baisse++;
+    }
+    return(nb);
 }
 
 int main(int argc, char const *argv[])
 {
-    ft_iterative_power()
-    return 0;
+   printf("0:%d\n", ft_iterative_power(1, -10));
+	printf("0:%d\n", ft_iterative_power(1, -1));
+	printf("1:%d\n", ft_iterative_power(10, 0));
+	printf("10:%d\n", ft_iterative_power(10, 1));
+	printf("100:%d\n", ft_iterative_power(10, 2));
+	printf("216:%d\n", ft_iterative_power(6, 3));
 }
