e1f0c59b31e6de91679bbddc5393636687c0a762
Wbelyne <joaquim1810@gmail.com>
2023-12-20 17:27:27 +0100

    progname

--- ft_find_next_prime.c
+++ ft_find_next_prime.c
@@ -1,5 +1,17 @@
 #include "lib.h"
-
+int ft_is_prime(int nb)
+{   
+	int deb = 2;
+    if (nb < 2)
+        return 0;
+	while (deb <= nb / deb)
+	{
+		if (nb % deb == 0)
+			return 0;
+		deb++;
+	}
+	return 1;	
+}
 
 
 int ft_find_next_prime(int nb)
---
+++ ft_print_program_name.c
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+int main(int argc, char *argv[])
+{
+    ft_putstr(argv[0]);
+    return 0;
+}
